a7dd6bb56e92550bef28d5941ed6eaeec07e8c4a
lukeshotgun <64715746+LukeWingert@users.noreply.github.com>
2022-02-26 16:59:33 -0500

    init

---
+++ Collision/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Sprint0.Collision
+{
+    class SortSweep : ICollision 
+    {
+        Game1 myGame;
+        private List<CollisionObject> xList; 
+        SortSweep(Game1 g) {
+            myGame = g; 
+        }
+        public void HandleCollisions()
+        {
+            loadLists()
+            sortX()
+            sortY()
+            assignHandlers();
+            return; 
+        }
+
+        private void loadLists()
+
+    }
+}
---
+++ ICollision.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    interface ICollision 
+    {
+        void HandleCollisions(); 
+    }
+}
