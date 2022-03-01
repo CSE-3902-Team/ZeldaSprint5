f7fe2dc139b39d1da3e22f51ea2e1bb95e7fa821
lukeshotgun <64715746+LukeWingert@users.noreply.github.com>
2022-02-28 19:03:08 -0500

    ...

--- Collision/SortSweep.cs
+++ Collision/SortSweep.cs
@@ -32,7 +32,6 @@ namespace Sprint0.Collision
 
         private int SortX(IBoxCollider obj1, IBoxCollider obj2)
         {
-            //Sort all colliders along the x-axis
             return obj1.CollisionBox.X.CompareTo(obj2.CollisionBox.X);
         }
 
@@ -43,6 +42,9 @@ namespace Sprint0.Collision
             //Once you find a beginning value that is greater than the old
             //one you can flush the list and add it to the list of collisions
             //that need to be handled. nested lists? if length is 1 no collisions
+
+            //Probably need two nested lists for x and y. then I just combine
+            //them to see what they have in common? then process them
             foreach(IBoxCollider colObj : boxColliders)
             {
                  
@@ -53,6 +55,16 @@ namespace Sprint0.Collision
         {
             //Sort all colliders along the y-axis
             return obj1.CollisionBox.Y.CompareTo(obj2.CollisionBox.Y);
+        }
+
+        private void FindCollisionsY()
+        { 
+            /*See if the collision is inside the x collision list
+            //if it is inside the list add y to the final list probably
+            want to the clear the x list afterwards.This is really goofy
+            with nested lists though
+            */
+        
         }
 
         private void AssignHandlers()
