52cc1b4a7f946ce791fb3c984b14387b3e8745c9
Craig-Cirino <ccirino103199@gmail.com>
2022-03-02 19:16:58 -0500

    started implementing CollisionHandlers

--- CollisionHandlers/CollisionHandlerPlayerBlock.cs
+++ CollisionHandlers/CollisionHandlerPlayerBlock.cs
@@ -6,6 +6,19 @@ namespace Sprint0.CollisionHandlers
 {
     public class CollisionHandlerPlayerBlock : ICollisionHandler
     {
+        private Player player;
+        private ITile block;
+        //private enum CollisionsDirections collisionsDirection;
+        private int overlap;
+        private int xDirection;
+        private int yDirection;
+        public CollisionHandlerPlayerBlock(Player player, ITile block, int overlap)
+        {
+            this.player = player;
+            this.block = block;
+            this.overlap = overlap;
+            //this.collisionDirection = collisionDirection;
+        }
         public void HandleCollision()
         {
             
