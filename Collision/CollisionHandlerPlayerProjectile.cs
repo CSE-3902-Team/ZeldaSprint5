d96a66c9f9e7e1a0915f4d1845ab846b181ae5b8
Craig-Cirino <ccirino103199@gmail.com>
2022-03-03 18:49:10 -0500

    implemented player collision handlers

--- Collision/CollisionHandlerPlayerProjectile.cs
+++ Collision/CollisionHandlerPlayerProjectile.cs
@@ -1,14 +1,55 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sprint0.Collision
 {
-    class CollisionHandlerPlayerProjectile : ICollisionHandler
+    public class CollisionHandlerPlayerProjectile : ICollisionHandler
     {
+        private Player player;
+        private IProjectile projectile;
+        private int currentDirection;
+        private CollisionDirections collisionDirections;
+        public CollisionHandlerPlayerProjectile(Player player, IProjectile projectile, CollisionDirections collisionDirections)
+        {
+            this.player = player;
+            this.projectile = projectile;
+            this.collisionDirections = collisionDirections;
+
+        }
         public void HandleCollision()
         {
+            float xDirection;
+            float yDirection;
 
+            switch (collisionDirections)
+            {
+                case CollisionDirections.North:
+                    yDirection = 1;
+                    xDirection = 0;
+                    break;
+                case CollisionDirections.East:
+                    yDirection = 0;
+                    xDirection = -1;
+                    break;
+                case CollisionDirections.South:
+                    yDirection = -1;
+                    xDirection = 0;
+                    break;
+                case CollisionDirections.West:
+                    yDirection = 0;
+                    xDirection = 1;
+                    break;
+                default:
+                    yDirection = 0;
+                    xDirection = 0;
+                    break;
+            }
+
+            Console.WriteLine("yDirection=" + yDirection);
+            projectile.IsRunning = false;
+            // player.State = new PlayerInjuredState(xDirection, yDirection);
         }
     }
 }
