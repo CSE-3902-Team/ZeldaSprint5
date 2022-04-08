5f51625fa56f754509199f2d131a69e284b06924
Craig-Cirino <ccirino103199@gmail.com>
2022-04-08 10:51:31 -0400

    added x's before the counts in HUD

--- HUD.cs
+++ HUD.cs
@@ -21,6 +21,9 @@ namespace Sprint0
         private Rectangle heartDestRect;
         private Rectangle currentHeartNeeded;
         private Rectangle numberSourceRect;
+        private Rectangle rupeeNumberDestRect;
+        private Rectangle bombNumberDestRect;
+        private Rectangle keyNumberDestRect;
 
         private int health;
         private int heartContainerCount;
@@ -39,11 +42,12 @@ namespace Sprint0
         const int numberHeight = 37;
         const int spaceBetweenNumbers = 5;
 
-        const int heartAndNumberYLocation = 255;
-        const int numberXLocation = 213;
-        const int rupeeYLocation = 73;
-        const int keyYLocation = 146;
-        const int bombYLocation = 182;
+        const int heartAndNumberYSourceLocation = 255;
+        const int numberXSourceLocation = 213;
+        const int rupeeYDestLocation = 73;
+        const int keyYDestLocation = 146;
+        const int bombYDestLocation = 182;
+        const int numberXDestLocation = 383;
 
         public HUD(Player player, SpriteBatch spritebatch, Texture2D headsUpDisplay)
         {
@@ -65,8 +69,13 @@ namespace Sprint0
             emptyHeartSourceRect = new Rectangle((heartWidth * 0) + (spaceBetweenHearts*0), 255, heartWidth, heartHeight);
             halfHeartSourceRect = new Rectangle((heartWidth * 1) + (spaceBetweenHearts * 1), 255, heartWidth, heartHeight);
             heartSourceRect = new Rectangle((heartWidth * 2) + (spaceBetweenHearts * 2), 255, heartWidth, heartHeight);
+            numberSourceRect = new Rectangle(numberXSourceLocation, heartAndNumberYSourceLocation, numberWidth, numberHeight);
 
             heartDestRect = new Rectangle(650, 146, heartWidth, heartHeight);
+            rupeeNumberDestRect = new Rectangle(numberXDestLocation, rupeeYDestLocation, numberWidth + 1, numberHeight);
+            bombNumberDestRect = new Rectangle(numberXDestLocation, bombYDestLocation, numberWidth + 1, numberHeight);
+            keyNumberDestRect = new Rectangle(numberXDestLocation, keyYDestLocation, numberWidth + 1, numberHeight);
+
         }
 
         public void Update()
@@ -83,10 +92,9 @@ namespace Sprint0
             Update();
             spriteBatch.Begin();
             spriteBatch.Draw(headsUpDisplay, hudRectangle, hudRectangle, Color.White);
-
-           // for(int i = 0; i < 3; i++)
-          //  {
-         //   }
+            spriteBatch.Draw(headsUpDisplay, rupeeNumberDestRect, numberSourceRect, Color.White);
+            spriteBatch.Draw(headsUpDisplay, bombNumberDestRect, numberSourceRect, Color.White);
+            spriteBatch.Draw(headsUpDisplay, keyNumberDestRect, numberSourceRect, Color.White);
 
             int remainingHalfHearts = health;
             for (int i = 0; i < maxHeartCount; i++)
